d2a7569c7974bc76baf3116c04b7389f1039f9ae
amitil <biloholovskyi>
2020-03-15 16:58:32 +0200

    add vendors

---
+++ gimranov/js/main.js
@@ -0,0 +1,27 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:48,
+    nav:true,
+    dots:false,
+    center:true,
+    autoWidth:true,
+    items:1,
+    responsive:{
+        0:{
+            dots:true
+        },
+        1000:{
+            dots:false
+        },               
+    }
+});
+
+    $(document).ready(() => {
+      $('.catalog-wrap').masonry({
+        // options
+        itemSelector: ".catalog__block", 
+        columnWidth: ".grid-sizer", 
+        percentPosition: true, 
+        horizontalOrder: true
+      });
+    });
