01876172499259df7503b7f94bdd392963830e09
amitil <biloholovskyi>
2020-03-16 21:39:15 +0200

    check work

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'ilgiz' );
+define( 'DB_NAME', 'gimranow' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'amitil' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'amitil' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
